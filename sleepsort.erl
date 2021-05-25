95f167dc0f84aed2a45d1cdc6beaefdd468af79c
Peter Stapleton <peter.stapleton@protonmail.com>
2021-05-24 19:51:38 -0700

    Now works with negative numbers. Removed helper function in favor of list comprehensions

--- sleepsort.erl
+++ sleepsort.erl
@@ -15,18 +15,27 @@
 
 %% API
 -spec sort(list(number())) -> list(number).
-sort([]) -> [];
 sort(L) when is_list(L) ->
-    Ref = make_ref(),
-    N = length(L),
     S = self(),
-    [spawn(fun() -> erlang:send_after(X * ?ACCURACY_FACTOR, S, {Ref, X}) end) || X <- L],
-    sort_(Ref, [], N).
 
-%% Private
--spec sort_(reference(), list(number()), number()) -> list(number()).
-sort_(_Ref, Acc, 0) -> lists:reverse(Acc);
-sort_(Ref, Acc, N) -> receive {Ref, X} -> sort_(Ref, [X|Acc], N - 1) end.
+    %% Partition the positive and negative numbers 
+    Neg = [X || X <- L, X < 0],
+    Pos = [X || X <- L, X >= 0],
+
+    F = fun(X, Ref) -> erlang:send_after(abs(X) * ?ACCURACY_FACTOR, S, {Ref, X}) end,
+    
+    %% spawn and collect positive first, then negative
+    PosRef = make_ref(),
+    NegRef = make_ref(),   
+
+    [spawn(fun() -> F(X, PosRef) end) || X <- Pos],    
+    PosSorted = [receive {PosRef, X} -> X end || _ <- Pos],
+    
+    [spawn(fun() -> F(X, NegRef) end) || X <- Neg],    
+    NegSorted = [receive {NegRef, X} -> X end || _ <- Neg],
+    
+    %% negatives received in reverse order
+    lists:reverse(NegSorted) ++ PosSorted.
 
 
 %% EUNIT
@@ -56,6 +65,12 @@ rand_test() ->
     %% decided by [random:uniform(20) || _ <- [1,2,3,4,5,6,7,8,9,10]],
     Input = [2,12,20,7,4,5,1,18,17,17], 
     Expected = lists:sort(Input),
+    Actual = sort(Input),
+    ?assertEqual(Expected, Actual).
+
+neg_test() ->
+    Input = [1,2,3,4,-4,-3,-2,-1,0],
+    Expected = [-4,-3,-2,-1,0,1,2,3,4],
     Actual = sort(Input),
     ?assertEqual(Expected, Actual).
 -endif.
